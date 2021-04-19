22d1292cbb2cd06442b05443749e4e389d3c7451
nconn711 <noahconner711@gmail.com>
2021-04-19 16:24:32 -0500

    changing bit resolution floating->fixed

--- MNIST_NN/sv_files/neurons.sv
+++ MNIST_NN/sv_files/neurons.sv
@@ -9,7 +9,7 @@ module neuron_784_20 (
 	logic [15:0] x, w;
 	
 	always_ff @ (posedge Clk) begin
-		Z = Active ? (accumulator >> 13) : Z;
+		Z = Active ? (accumulator >> 11) : Z;
 	end
 	
 	always_comb begin
@@ -41,7 +41,7 @@ module neuron_20_20 (
 	logic [15:0] x, w;
 	
 	always_ff @ (posedge Clk)
-		Z = Active ? (accumulator >> 13) : Z;
+		Z = Active ? (accumulator >> 11) : Z;
 	
 	always_comb begin
 		sload = 1'b0;
@@ -72,7 +72,7 @@ module neuron_20_10 (
 	logic [15:0] x, w;
 	
 	always_ff @ (posedge Clk)
-		Z = Active ? (accumulator >> 13) : Z;
+		Z = Active ? (accumulator >> 11) : Z;
 	
 	always_comb begin
 		sload = 1'b0;
