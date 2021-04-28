08340006057672b38ccfc56cf1e6ddb06e229f90
Adam Naboulsi <ajnaboulsi@gmail.com>
2021-04-27 23:06:41 -0500

    changed ram/rom instantiate modules to verilog

---
+++ MNIST_NN/mem_files/ram_single.v
@@ -0,0 +1,19 @@
+//Code for inferred RAM 2^10 address space x 16 bits
+module ram #(parameter ADDR_WIDTH=10, DATA_WIDTH=16, ADDR_SIZE=1024, FILE_NAME=" ")
+    (q, d, a, we, clk)
+    output [DATA_WIDTH-1:0] q;
+    input logic [DATA_WIDTH-1:0] d;
+    input logic [ADDR_WIDTH-1:0] a;
+    input logic we, clk);
+     
+   reg [DATA_WIDTH-1:0] mem [ADDR_SIZE-1:0];
+   initial begin
+    $readmemh(FILE_NAME, mem);
+   end
+
+    always @(posedge clk) begin
+        if (we)
+            mem[a] <= d;
+        q <= mem[a];
+   end
+endmodule
---
+++ MNIST_NN/mem_files/rom_single.v
@@ -0,0 +1,16 @@
+//Code for inferred ROM 2^16 address space x 16 bits
+module rom #(parameter ADDR_WIDTH=16, DATA_WIDTH=16, ADDR_SIZE=65536, FILE_NAME=" ")
+    (q, d, a, we, clk)
+    output [DATA_WIDTH-1:0] q;
+    input logic [DATA_WIDTH-1:0] d;
+    input logic [ADDR_WIDTH-1:0] a;
+    input logic we, clk);
+
+	reg [DATA_WIDTH-1:0] mem [ADDR_SIZE-1:0];
+   initial begin
+    $readmemh(FILE_NAME, mem);
+   end
+    always @(posedge clk) begin
+        q <= mem[a];
+   end
+endmodule
