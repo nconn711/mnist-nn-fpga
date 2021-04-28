a1a19b845595e0ca0d066815c187d07223bedcd7
Adam Naboulsi <ajnaboulsi@gmail.com>
2021-04-27 22:51:49 -0500

    adjusted color_mapper module

--- MNIST_NN/sv_files/Color_Mapper.sv
+++ MNIST_NN/sv_files/Color_Mapper.sv
@@ -13,51 +13,69 @@
 //-------------------------------------------------------------------------
 
 
-module  color_mapper ( input        [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
-                       output logic [7:0]  Red, Green, Blue );
-    
-    logic ball_on;
-	 
- /* Old Ball: Generated square box by checking if the current pixel is within a square of length
-    2*Ball_Size, centered at (BallX, BallY).  Note that this requires unsigned comparisons.
-	 
-    if ((DrawX >= BallX - Ball_size) &&
-       (DrawX <= BallX + Ball_size) &&
-       (DrawY >= BallY - Ball_size) &&
-       (DrawY <= BallY + Ball_size))
+module  color_mapper( 
+    input [9:0] BallX, BallY, DrawX, DrawY, Ball_size,
+    input [15:0]reg [27:0][27:0]
+    output logic [7:0]  Red, Green, Blue 
+);
 
-     New Ball: Generates (pixelated) circle by using the standard circle formula.  Note that while 
-     this single line is quite powerful descriptively, it causes the synthesis tool to use up three
-     of the 12 available multipliers on the chip!  Since the multiplicants are required to be signed,
-	  we have to first cast them from logic to int (signed by default) before they are multiplied). */
-	  
+    logic ball_on;
+    logic canvas_on;
+    logic X_Block;
+    logic Y_Block;
+
     int DistX, DistY, Size;
-	 assign DistX = DrawX - BallX;
+	assign DistX = DrawX - BallX;
     assign DistY = DrawY - BallY;
     assign Size = Ball_size;
-	  
-    always_comb
-    begin:Ball_on_proc
-        if ( ( DistX*DistX + DistY*DistY) <= (Size * Size) ) 
+
+    always_comb begin:RGB_Display
+        //Ball_on_proc
+        if ((DistX*DistX + DistY*DistY) <= (Size * Size)) 
             ball_on = 1'b1;
         else 
             ball_on = 1'b0;
-     end 
-       
-    always_comb
-    begin:RGB_Display
-        if ((ball_on == 1'b1)) 
-        begin 
-            Red = 8'hff;
-            Green = 8'h55;
-            Blue = 8'h00;
-        end       
+
+        //Calc BlockX/Y
+        for (int x=0; x<28; x++) begin
+            for (int y=0; y<28; y++) begin
+                if(DrawX >= x*14 && DrawX < (x+1)*14) 
+                    X_Block = x;
+                else
+                    X_Block = 32;
+
+                if(DrawY >= y*14 && DrawY < (y+1)*14)
+                    Y_Block = x;
+                else
+                    Y_Block = 32;
+            end
+        end
+
+        //Canvas_on_proc
+        if (X_Block != 32 && Y_Block != 32) 
+            canvas_on = 1'b1;
         else 
-        begin 
-            Red = 8'h00; 
-            Green = 8'h00;
-            Blue = 8'h7f - DrawX[9:3];
-        end      
+            canvas_on = 1'b0;
+
+        //Set RGB values of Display
+        if ((ball_on == 1'b1)) 
+            begin 
+                Red = 8'hff;
+                Green = 8'h00;
+                Blue = 8'h00;
+            end       
+        else if ((canvas_on == 1'b1)) 
+            begin
+                Red = reg[X_Block][Y_Block][11:4];
+                Green = reg[X_Block][Y_Block][11:4];
+                Blue = reg[X_Block][Y_Block][11:4];
+            end
+        else
+            begin 
+                Red = 8'h00; 
+                Green = 8'h00;
+                Blue = 8'h00;
+            end      
     end 
     
 endmodule
@@ -97,7 +115,7 @@ module canvas(
                 end
             else 
                 begin
-                    if (X_Pos >= 199 && X_Pos <= 591 && Y_Pos >= 43 && Y_Pos <= 435) //or X_Block/Y_Block == 32
+                    if (X_Block != 32 && Y_Block != 32) //or X_Block/Y_Block == 32
                         reg[X_Block][Y_Block] <= reg[X_Block][Y_Block] + 2000;
                 end
         end
